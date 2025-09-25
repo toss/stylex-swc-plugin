c3b1b169ae26bf8026b809afb421f08f09f1f351
Suh Donghwi <suhdonghwi@toss.im>
2025-09-25 22:10:45 +0900

    refactor: resolve_file_path logic simplifying

--- crates/stylex-path-resolver/src/resolvers/mod.rs
+++ crates/stylex-path-resolver/src/resolvers/mod.rs
@@ -1,10 +1,6 @@
-use log::debug;
 use oxc_resolver::{AliasValue, ResolveOptions, Resolver};
-use path_clean::PathClean;
 use rustc_hash::FxHashMap;
 use std::path::{Path, PathBuf};
-
-use std::fs;
 
 mod tests;
 
@@ -16,102 +12,52 @@ pub fn resolve_file_path(
   root_path: &str,
   aliases: &FxHashMap<String, Vec<String>>,
 ) -> std::io::Result<PathBuf> {
-  let source_file_dir = Path::new(source_file_path).parent().unwrap();
-  let root_path = Path::new(root_path);
-
-  let cwd_path = Path::new(root_path);
-
-  let resolved_file_paths = {
-    let resolver_options = ResolveOptions {
-      alias: aliases
-        .iter()
-        .map(|(alias, values)| {
-          (
-            alias.clone(),
-            values
-              .iter()
-              .map(|value| AliasValue::from(value.clone()))
-              .collect(),
-          )
-        })
-        .collect(),
-      extensions: EXTENSIONS.iter().map(|ext| ext.to_string()).collect(),
-      condition_names: vec![
-        "node".into(),
-        "import".into(),
-        "require".into(),
-        "default".into(),
-      ],
-      ..ResolveOptions::default()
-    };
-
-    match Resolver::new(resolver_options).resolve(source_file_dir, &import_path_str) {
-      Err(err) => {
-        println!("err: {:?}", err);
-        vec![]
-      }
-      Ok(resolution) => {
-        println!("resolution: {:?}", resolution);
-        vec![resolution.into_path_buf()]
-      }
-    }
+  let resolver_options = ResolveOptions {
+    extensions: EXTENSIONS.iter().map(|ext| ext.to_string()).collect(),
+    condition_names: vec![
+      "node".into(),
+      "import".into(),
+      "require".into(),
+      "default".into(),
+    ],
+    ..ResolveOptions::default()
   };
 
-  let resolved_potential_file_paths = resolved_file_paths
-    .iter()
-    .filter(|path| path.as_path() != Path::new("."))
-    .collect::<Vec<&PathBuf>>();
+  let resolver_without_aliases = Resolver::new(resolver_options.clone());
+  let resolver_with_aliases = Resolver::new(ResolveOptions {
+    alias: aliases
+      .iter()
+      .map(|(alias, values)| {
+        (
+          alias.clone(),
+          values
+            .iter()
+            .map(|value| AliasValue::from(value.clone()))
+            .collect(),
+        )
+      })
+      .collect(),
+    ..resolver_options
+  });
 
-  debug!(
-    "Resolved potential paths: {:?} for import `{}`",
-    resolved_potential_file_paths, import_path_str
-  );
+  let source_directory_path = Path::new(source_file_path)
+    .parent()
+    .unwrap()
+    .to_str()
+    .unwrap();
 
-  for ext in EXTENSIONS.iter() {
-    for resolved_file_path in resolved_potential_file_paths.iter() {
-      let mut resolved_file_path = resolved_file_path.clean();
-
-      if let Some(extension) = resolved_file_path.extension() {
-        let subpath = extension.to_string_lossy();
-        if EXTENSIONS
-          .iter()
-          .all(|ext| !ext.ends_with(subpath.as_ref()))
-        {
-          resolved_file_path.set_extension(format!("{}{}", subpath, ext));
-        }
-      } else {
-        resolved_file_path.set_extension(ext.trim_start_matches("."));
-      }
-
-      let cleaned_path = resolved_file_path.to_string_lossy().to_string();
-
-      let path_to_check: PathBuf;
-      let node_modules_path_to_check: PathBuf;
-
-      if !cleaned_path.contains(root_path.to_str().expect("root path is not valid")) {
-        if !cleaned_path.starts_with("node_modules") {
-          node_modules_path_to_check = cwd_path.join("node_modules").join(&cleaned_path);
-        } else {
-          node_modules_path_to_check = cwd_path.join(&cleaned_path);
-        }
-        path_to_check = cwd_path.join(cleaned_path);
-      } else {
-        path_to_check = PathBuf::from(&cleaned_path);
-        node_modules_path_to_check = path_to_check.clone();
-      }
-
-      if fs::metadata(&path_to_check).is_ok() {
-        return Ok(path_to_check.to_path_buf().clean());
-      }
-
-      if fs::metadata(&node_modules_path_to_check).is_ok() {
-        return Ok(node_modules_path_to_check.to_path_buf().clean());
-      }
-    }
+  if import_path_str.starts_with(".")
+    && let Ok(resolution) =
+      resolver_without_aliases.resolve(source_directory_path, &import_path_str)
+  {
+    return Ok(resolution.into_path_buf());
   }
 
-  Err(std::io::Error::new(
-    std::io::ErrorKind::NotFound,
-    "File not found",
-  ))
+  match resolver_with_aliases.resolve(source_file_path, &import_path_str) {
+    Ok(resolution) => Ok(resolution.into_path_buf()),
+    Err(_) => Err(std::io::Error::new(
+      std::io::ErrorKind::NotFound,
+      "File not found",
+    )),
+  }
 }
